9cf51758befbcdf5fe10d262ba97ebef768f1f7a
Zolotarev P <pvzolotarev@1cbit.ru>
2026-07-30 13:08:22 +0300

    hmi

--- app/hmi/Data_granary/scripts/mechanisms.js
+++ app/hmi/Data_granary/scripts/mechanisms.js
@@ -19,8 +19,8 @@ Mechanism.prototype.setEnable = function(enable) {
 
 // --- Engine ---
 
-function Engine(name) {
-	Mechanism.call(this, name)
+function Engine(name, title) {
+	Mechanism.call(this, name, title)
 }
 Engine.prototype = Object.create(Mechanism.prototype)
 Engine.prototype.constructor = Engine
@@ -39,8 +39,8 @@ Engine.prototype.className = function() {
 
 // --- Valve ---
 
-function Valve(name) {
-	Mechanism.call(this, name)
+function Valve(name, title) {
+	Mechanism.call(this, name, title)
 }
 Valve.prototype = Object.create(Mechanism.prototype)
 Valve.prototype.constructor = Valve
@@ -62,54 +62,55 @@ Valve.prototype.className = function() {
 function init() {
 
 	mechanisms['m1_2']  = new Engine('m1_2', 'Конвейер 1.2')
-	mechanisms['m1_3']  = new Engine('m1_3')
-	mechanisms['m3_4']  = new Engine('m3_4')
-	mechanisms['m13_1'] = new Engine('m13_1')
-	mechanisms['m3_5']  = new Engine('m3_5')
-	mechanisms['m3_6']  = new Engine('m3_6')
-	mechanisms['m6_10'] = new Engine('m6_10')
-	mechanisms['m6_9']  = new Engine('m6_9')
-	mechanisms['m3_3']  = new Engine('m3_3')
-	mechanisms['m14']   = new Engine('m14')
-	mechanisms['m13_2'] = new Engine('m13_2')
-	mechanisms['m13_3'] = new Engine('m13_3')
-	mechanisms['m13_4'] = new Engine('m13_4')
-	mechanisms['m13_5'] = new Engine('m13_5')
-	mechanisms['m13_6'] = new Engine('m13_6')
-	mechanisms['m13_7'] = new Engine('m13_7')
-	mechanisms['m13_8'] = new Engine('m13_8')
-	mechanisms['m1_4']  = new Engine('m1_4')
-	mechanisms['m6_1']  = new Engine('m6_1')
-	mechanisms['m1_8']  = new Engine('m1_8')
-	mechanisms['m1_7']  = new Engine('m1_7')
-	mechanisms['m6_2']  = new Engine('m6_2')
-	mechanisms['m1_6']  = new Engine('m1_6')
-	mechanisms['m1_5']  = new Engine('m1_5')
-	mechanisms['m1_9']  = new Engine('m1_9')
-	mechanisms['m1_10'] = new Engine('m1_10')
-	mechanisms['m1_11'] = new Engine('m1_11')
-	mechanisms['m1_12'] = new Engine('m1_12')
-	mechanisms['m6_5']  = new Engine('m6_5')
-	mechanisms['m6_11'] = new Engine('m6_11')
-	mechanisms['m6_4']  = new Engine('m6_4')
-	mechanisms['m6_6']  = new Engine('m6_6')
-	mechanisms['m6_8']  = new Engine('m6_8')
-	mechanisms['m16_3'] = new Engine('m16_3')
-	mechanisms['m15_3'] = new Engine('m15_3')
-	mechanisms['m6_7']  = new Engine('m6_7')
-	mechanisms['m8_9']  = new Valve('m8_9', 'Клапан 8.9')
-	mechanisms['m8_8']  = new Valve('m8_8')
-	mechanisms['m8_2']  = new Valve('m8_2')
-	mechanisms['m8_7']  = new Valve('m8_7')
-	mechanisms['m8_12'] = new Valve('m8_12')
-	mechanisms['m8_13'] = new Valve('m8_13')
-	mechanisms['m8_10'] = new Valve('m8_10')
-	mechanisms['m8_11'] = new Valve('m8_11')
-	mechanisms['m8_1']  = new Valve('m8_1', 'Клапан 8.1')
-	mechanisms['m8_5']  = new Valve('m8_5')
-	mechanisms['m8_6']  = new Valve('m8_6')
-	mechanisms['m8_3']  = new Valve('m8_3')
-	mechanisms['m8_4']  = new Valve('m8_4')
+	mechanisms['m1_3']  = new Engine('m1_3', 'Конвейер 1.3')
+	mechanisms['m1_4']  = new Engine('m1_4', 'Конвейер 1.4')
+	mechanisms['m1_5']  = new Engine('m1_5', 'Конвейер 1.5')
+	mechanisms['m1_6']  = new Engine('m1_6', 'Конвейер 1.6')
+	mechanisms['m1_7']  = new Engine('m1_7', 'Конвейер 1.7')
+	mechanisms['m1_8']  = new Engine('m1_8', 'Конвейер 1.8')
+	mechanisms['m1_9']  = new Engine('m1_9', 'Конвейер 1.9')
+	mechanisms['m1_10'] = new Engine('m1_10', 'Конвейер 1.10')
+	mechanisms['m1_11'] = new Engine('m1_11', 'Конвейер 1.11')
+	mechanisms['m1_12'] = new Engine('m1_12', 'Конвейер 1.12')
+	mechanisms['m13_1'] = new Engine('m13_1', 'Вентилятор 13.1')
+	mechanisms['m13_2'] = new Engine('m13_2', 'Вентилятор 13.2')
+	mechanisms['m13_3'] = new Engine('m13_3', 'Вентилятор 13.3')
+	mechanisms['m13_4'] = new Engine('m13_4', 'Вентилятор 13.4')
+	mechanisms['m13_5'] = new Engine('m13_5', 'Вентилятор 13.5')
+	mechanisms['m13_6'] = new Engine('m13_6', 'Вентилятор 13.6')
+	mechanisms['m13_7'] = new Engine('m13_7', 'Вентилятор 13.7')
+	mechanisms['m13_8'] = new Engine('m13_8', 'Вентилятор 13.8')
+	mechanisms['m14']   = new Engine('m14', 'Вентилятор 14')
+	mechanisms['m15_3'] = new Engine('m15_3', 'Вентилятор 15.3')
+	mechanisms['m16_3'] = new Engine('m16_3', 'Шлюзовой затвор 16.3')
+	mechanisms['m3_3']  = new Engine('m3_3', 'Нория 3.3')
+	mechanisms['m3_4']  = new Engine('m3_4', 'Нория 3.4')
+	mechanisms['m3_5']  = new Engine('m3_5', 'Нория 3.5')
+	mechanisms['m3_6']  = new Engine('m3_6', 'Нория 3.6')
+	mechanisms['m6_1']  = new Engine('m6_1', 'Конвейер 6.1')
+	mechanisms['m6_2']  = new Engine('m6_2', 'Конвейер 6.2')
+	mechanisms['m6_4']  = new Engine('m6_4', 'Конвейер 6.4')
+	mechanisms['m6_5']  = new Engine('m6_5', 'Конвейер 6.5')
+	mechanisms['m6_6']  = new Engine('m6_6', 'Конвейер 6.6')
+	mechanisms['m6_7']  = new Engine('m6_7', 'Конвейер 6.7')
+	mechanisms['m6_8']  = new Engine('m6_8', 'Конвейер 6.8')
+	mechanisms['m6_9']  = new Engine('m6_9', 'Конвейер 6.9')
+	mechanisms['m6_10'] = new Engine('m6_10', 'Конвейер 6.10')
+	mechanisms['m6_11'] = new Engine('m6_11', 'Конвейер 6.11')
+	mechanisms['m8_1']  = new Valve('m8_1', 'Задвижка 8.1')
+	mechanisms['m8_2']  = new Valve('m8_2', 'Задвижка 8.2')
+	mechanisms['m8_3']  = new Valve('m8_3', 'Задвижка 8.3')
+	mechanisms['m8_4']  = new Valve('m8_4', 'Задвижка 8.4')
+	mechanisms['m8_5']  = new Valve('m8_5', 'Задвижка 8.5')
+	mechanisms['m8_6']  = new Valve('m8_6', 'Задвижка 8.6')
+	mechanisms['m8_7']  = new Valve('m8_7', 'Задвижка 8.7')
+	mechanisms['m8_8']  = new Valve('m8_8', 'Задвижка 8.8')
+	mechanisms['m8_9']  = new Valve('m8_9', 'Задвижка 8.9')
+	mechanisms['m8_10'] = new Valve('m8_10', 'Задвижка 8.10')
+	mechanisms['m8_11'] = new Valve('m8_11', 'Задвижка 8.11')
+	mechanisms['m8_12'] = new Valve('m8_12', 'Задвижка 8.12')
+	mechanisms['m8_13'] = new Valve('m8_13', 'Задвижка 8.13')
+
 }
 
 
@@ -141,21 +142,24 @@ function showDlg(mech_name) {
 	getGlobalVar('dlg_timeout').setBindSpeaker(current.timeout)
 	getGlobalVar('dlg_state').setBindSpeaker(current.state)
 	getGlobalVar('dlg_name').setStringData(current.title)
-//	getScreen('engine_dlg').showNormal();
 
     if (current.className() == 'Valve') {
         getGlobalVar('dlg_pusk_btn').setStringData('Открыть')
         getGlobalVar('dlg_stop_btn').setStringData('Закрыть')
+        getGlobalVar('dlg_sensor_name').setStringData('Концевики')
         getGlobalVar('dlg_is_valve').setBooleanData(true)
         getScreen('engine_dlg').getObject('lbl_state_valve').show()
         getScreen('engine_dlg').getObject('lbl_state_eng').hide()
+        getScreen('engine_dlg').getObject('di_closed').show()
     }
 	else {
         getGlobalVar('dlg_pusk_btn').setStringData('Пуск')
         getGlobalVar('dlg_stop_btn').setStringData('Стоп')
+        getGlobalVar('dlg_sensor_name').setStringData('Работа')
         getGlobalVar('dlg_is_valve').setBooleanData(false)
         getScreen('engine_dlg').getObject('lbl_state_valve').hide()
         getScreen('engine_dlg').getObject('lbl_state_eng').show()
+        getScreen('engine_dlg').getObject('di_closed').hide()
     }
 }
 
